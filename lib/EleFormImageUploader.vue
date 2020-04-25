012132604dab33595dfb993a3a52924c24db7b32
zhangchaojie <1098626505@qq.com>
2020-04-25 20:29:38 +0800

    chore: 增加 cdn 引入方式

--- lib/EleFormImageUploader.vue
+++ lib/EleFormImageUploader.vue
@@ -14,7 +14,7 @@ import formMixin from 'vue-ele-form/lib/mixins/formMixin'
 import uploadMixin from 'vue-ele-form/lib/mixins/uploadMixin'
 
 export default {
-  name: 'EleFormImage',
+  name: 'ele-form-image-uploader',
   mixins: [formMixin, uploadMixin],
   components: {
     EleUploadImage
