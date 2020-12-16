f6b9ba2c211cc6fd5e98286d29a1ea5171362c29
超杰 <1098626505@qq.com>
2020-12-16 22:57:11 +0800

    fix:修复名称导致的全局属性无法设置的问题

--- lib/EleFormImageUploader.vue
+++ lib/EleFormImageUploader.vue
@@ -14,7 +14,7 @@ import formMixin from 'vue-ele-form/lib/mixins/formMixin'
 import uploadMixin from 'vue-ele-form/lib/mixins/uploadMixin'
 
 export default {
-  name: 'ele-form-image-uploader',
+  name: 'image-uploader',
   mixins: [formMixin, uploadMixin],
   components: {
     EleUploadImage
